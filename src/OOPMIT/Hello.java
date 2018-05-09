81cb2d66fd453d9756ce8b9014daad8826a77058
Dongurdee <noosai_fon@hotmail.com>
2018-05-09 10:28:02 +0700

    Hello

---
+++ src/OOPMIT/Hello.java
@@ -0,0 +1,11 @@
+package OOPMIT;
+
+import org.omg.Messaging.SYNC_WITH_TRANSPORT;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello, My name is Dongurdee");
+        System.out.print("Major : Information System");
+        System.out.print("Faculty : Mangment Technology");
+    }
+}
