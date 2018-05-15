9dd9d649561d1410a8bc3cb822141289defe6038
Dongurdee <noosai_fon@hotmail.com>
2018-05-15 13:58:54 +0700

    StudentApp

--- src/lab7/studentApp.java
+++ src/lab7/studentApp.java
@@ -1,5 +1,7 @@
 package lab7;
 
+import java.util.Scanner;
+
 public class studentApp {
     public static void main(String[] args) {
         Student s1 = new Student();
@@ -14,10 +16,20 @@ public class studentApp {
         //intput data from user
         Student s3 = new Student()
         s3 = inputdata(s3);
-    }
+        System.out.print(s3.toString());
+    }//main
 
-    private static Student inputdata(Student s3) {
-
-
+    private static Student inputdata(Student s) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("please enter your infromation : ");
+        System.out.print("name : ");
+        s.setName(nextLione());
+        System.out.print("Age : ");
+        s.setAge(sc.nextInt());
+        System.out.print("Gender : ");
+        s.setGrnder(sc.nextLine());
+        System.out.print("Height : ");
+        s.setHeight(sc.nextInt());
+        return s;
     }
 }
