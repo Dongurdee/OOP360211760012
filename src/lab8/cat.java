30add7d5a92b1432a1d0ad03965def778cbd9911
Dongurdee <noosai_fon@hotmail.com>
2018-05-15 16:44:22 +0700

    StudentAppList55

---
+++ src/lab8/cat.java
@@ -0,0 +1,10 @@
+package lab8;
+
+public class cat extends Pet{
+        @Override
+        public void makencise() {
+            System.out.println("Weee Weee!!!");
+
+        }
+    }
+
---
+++ src/lab8/dog.java
@@ -0,0 +1,9 @@
+package lab8;
+
+public class dog  extends Pet{
+    @Override
+    public void makencise() {
+        System.out.println("BOX BOX!!!");
+
+    }
+}
